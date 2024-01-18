085ae347a6fdb28e3e14417da7adc16ce1174715
Hemant Thapa <104982185+AnalyticalHarry@users.noreply.github.com>
2024-01-18 19:23:43 +0000

    Update 7 StringConcatenation part 2.cpp

--- 7 StringConcatenation part 2.cpp
+++ 7 StringConcatenation part 2.cpp
@@ -5,6 +5,10 @@
 std::string concatenateStrings(const std::string stringOne, const std::string stringTwo) {
     return stringOne + stringTwo;
 }
+/*
+Void functions perform a task or a series of actions but do not produce a value that can be used in further computations.
+In the function declaration and definition, you specify void as the return type to indicate that the function is void and does not return a value.
+*/
 
 //function for the first set of concatenations, which will directly print output
 void firstSetOfConcatenation() {
