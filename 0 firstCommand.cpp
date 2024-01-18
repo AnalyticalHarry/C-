a6b3f90209b39a7e4d25676a68a74490d5d04ef8
Hemant Thapa <104982185+AnalyticalHarry@users.noreply.github.com>
2024-01-18 20:20:49 +0000

    Update 0 firstCommand.cpp

--- 0 firstCommand.cpp
+++ 0 firstCommand.cpp
@@ -1,9 +1,9 @@
-#include <iostream> //include the iostream header file, which allows for basic input and output operations.
+//include the iostream header file, which allows for basic input and output operations.
+#include <iostream> 
 
 //main function - the entry point of the program.
 int main(){
     //print the text "Hello World!" to the standard output (usually the console).
-    
     std::cout << "Hello World!" <<std::endl;
     std::cout<<"My first code in C++" <<std::endl;
     //return 0 to indicate successful program execution to the operating system.
