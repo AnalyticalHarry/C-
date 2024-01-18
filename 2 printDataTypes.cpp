33a0229b25999e021db3da6169e8f961eb10b6d2
Hemant Thapa <104982185+AnalyticalHarry@users.noreply.github.com>
2024-01-18 17:27:54 +0000

    Add files via upload

---
+++ 2 printDataTypes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+    //print string
+    std::cout << "Hello World!" << std::endl;
+
+    //print an integer
+    int num = 100;
+    std::cout << "The value of number is: "<< num << std::endl;
+
+    //print floating point number
+    double valueOfPi = 3.14;
+    std::cout << "Value of Pi is: "<< valueOfPi << std::endl;
+
+    //print character
+    char ch = 'A';
+    std::cout << "The character is: "<< ch << std::endl;
+
+    //print a boolean
+    bool isTrue = true;
+    std::cout <<"The boolean value is: " << isTrue << std::endl;
+
+    return 0;
+}
