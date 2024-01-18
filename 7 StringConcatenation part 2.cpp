d038739bd0f686eea8f14361ffb300ffd9f419c2
Hemant Thapa <104982185+AnalyticalHarry@users.noreply.github.com>
2024-01-18 19:07:37 +0000

    Create 7 StringConcatenation part 2.cpp

---
+++ 7 StringConcatenation part 2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string> 
+
+//function for concatenation
+std::string concatenateStrings(const std::string stringOne, const std::string stringTwo) {
+    return stringOne + stringTwo;
+}
+
+//function for the first set of concatenations, which will directly print output
+void firstSetOfConcatenation() {
+    std::string stringOne = "Hello";
+    std::string stringTwo = " World";
+    std::string combined = stringOne + stringTwo;
+    std::cout << combined << std::endl;
+}
+
+//function for the seconf set of concatenations, which will print through function
+void secondSetOfConcatenation() {
+    std::string stringOne = "My name is ";
+    std::string stringTwo = " Harry";
+    std::string combined = concatenateStrings(stringOne, stringTwo);
+    std::cout << combined << std::endl;
+}
+
+int main() {
+    //calling the functions to perform the concatenations
+    firstSetOfConcatenation();
+    secondSetOfConcatenation();
+    return 0;
+}
