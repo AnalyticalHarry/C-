04f48abe0be1d42730eb5f46e17cbdc5e9a2774f
Hemant Thapa <104982185+AnalyticalHarry@users.noreply.github.com>
2024-01-18 23:05:00 +0000

    Add files via upload

---
+++ 12 inputAndOutput part 3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+//iomanip header for setprecision
+#include <iomanip> 
+//cmath header for mathematical functions
+#include <cmath>   
+using namespace std;
+
+int main() {
+    //value of pi
+    const double pi = 3.14159265358979323846; 
+    //prompt the user for the radius of the circle
+    double radius;
+    cout << "Enter the radius of the circle: "; cin >> radius;
+    //area of the circle
+    double area = pi * pow(radius, 2);
+    //circumference (perimeter) of the circle
+    double circumference = 2 * pi * radius;
+    //results with precision set to 2 decimal places
+    //set precision to 4 decimal places
+    cout << fixed << setprecision(4); 
+    cout << "The area of the circle with radius " << radius << " is: " << area << endl;
+    cout << "The circumference of the circle with radius " << radius << " is: " << circumference << endl;
+    return 0;
+}
