94e918f86cc1a2f6a09b65692b6eeadcbfdbbb48
Hemant Thapa <104982185+AnalyticalHarry@users.noreply.github.com>
2024-01-19 00:25:37 +0000

    Add files via upload

---
+++ 16 forLoop part 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //for loop to count from 1 to 10
+    for (int i = 1; i <= 10; i++) {
+        cout << i << " "; 
+    }
+    cout << endl;
+    return 0;
+}
+
+//******************************************************
+// Author: Hemant Thapa
+// Programming Language: C++
+// Date Pushed to GitHub: 18.01.2024
+// Email: hemantthapa1998@gmail.com
+//******************************************************
